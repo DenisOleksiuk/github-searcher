cf1e0c3ac26d406b41b7e508a525e9cf36995007
Denis <66439220+DenisOleksiuk@users.noreply.github.com>
2023-01-29 11:37:30 +0200

    Task (#1)
    Co-authored-by: Denis Oleksiuk <d.oleksiuk@codeastrum.com>

--- src/pages/homePage.tsx
+++ src/pages/homePage.tsx
@@ -3,17 +3,18 @@ import { SearchBar } from '../components/SearchBar';
 import { UserList } from '../components/UserList';
 
 interface IHomePage {
-  users: Array<any>;
-  setValue: (str: string) => void;
+    users: Array<any>;
+    setValue: (str: string) => void;
 }
 
 const HomePage: React.FC<IHomePage> = ({ users, setValue }) => {
-  return (
-    <React.Fragment>
-      <SearchBar placeholder="Search for Users" searchHandler={setValue} />
-      <UserList users={users} />
-    </React.Fragment>
-  );
+    return (
+        <React.Fragment>
+            <SearchBar placeholder="Search for Users" searchHandler={setValue} />
+            <UserList users={users} />
+            test-1 test-2 test-3
+        </React.Fragment>
+    );
 };
 
 export default HomePage;
